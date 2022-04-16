35550cee8d13bfce12ed64185fafc01ccbbce107
sahrudin <sahrudinu48@gmail.com>
2022-04-16 11:39:49 +0700

    todo app initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+  import TodoList from './components/TodoList.vue'
+  export default {
+    components: {
+      TodoList
+    }
+  }
+</script>
+
+<template>
+  <main>
+    <TodoList></TodoList>
+  </main>
+</template>
+
+<style>
+  body{
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    background-color: rgb(17, 66, 20);
+    color: white;
+    box-sizing: border-box;
+  }
+  main{
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    data() {
+        return {
+            todo: '',
+            savedTodo: []
+        }
+    },
+    methods: {
+        saveTodo(){
+            this.savedTodo.push(this.todo)
+            this.todo = ''
+        },
+        deleteTodo(index){
+            this.savedTodo.splice(index, 1)
+        }
+    }
+}
+</script>
+
+<template>
+    <section>
+        <section>
+            <h1>Todo List App</h1>
+            <p>Create your own todolist</p>
+            <div>
+                <input v-model="todo" type="text">
+                <button @click="saveTodo">Save</button>
+            </div>
+        </section>
+        <section>
+            <div v-for="(todo, index) in savedTodo" :key="todo">
+                <div>{{ todo }}</div>
+                <button @click="deleteTodo(index)">Delete</button>
+            </div>
+        </section>
+    </section>
+</template>
+
+<style scoped>
+</style>
